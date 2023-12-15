aa89a459a210d62af06a107ae3892ec62f1c33e1
spiddanjuma <petedanj@gmail.com>
2023-12-14 22:39:33 -1200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'
+import Contact from './Contact.jsx'
+import Header from './component/header.jsx'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+   <Router>
+    <Routes>
+      <Route path='/' element={<Contact/>}></Route>
+      <Route path='/header' element={<Header/>}></Route>
+    </Routes>
+   </Router>
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/Contact.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Header from './component/header.jsx'
+function Contact() {
+  return (
+  <>
+
+<form class="container p-4 row g-3 mx-auto bg-info" style={{width:'18rem', marginTop:'8%'}}>
+<Header/>
+    <div class="col-12">
+     
+      <input placeholder='fullname' type="text" class="form-control" id="inputName"/>
+    </div>
+    <div class="col-12">
+      <input placeholder='email address' type="email" class="form-control" id="inputEmail"/>
+    </div>
+    <div class="col-12">
+  
+    <textarea placeholder='message us...' class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+    </div>
+   
+    <div class="col-12 mx-auto">
+      <button type="submit" class="btn col-6 btn-primary">Send</button>
+    </div>
+  </form>
+  </>
+  )
+}
+
+export default Contact
